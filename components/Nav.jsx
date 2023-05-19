1d0e768ea9fa323aefaa9dc0650a8f0d86d7eee7
Sakib Hasan <sakib100.sa@gmail.com>
2023-05-19 17:37:00 +0600

    working on navbar

---
+++ components/Nav.jsx
@@ -0,0 +1,42 @@
+'use client';
+import Link from 'next/link';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
+const Nav = () => {
+	const isUserLoggedIn = true;
+	return (
+		<nav className="flex-between w-full mb-16 pt-3">
+			<Link
+				href="/"
+				className="flex gap-2 flex-center">
+				<Image
+					src="/assets/images/logo.svg"
+					alt="logo"
+					width={30}
+					height={30}
+					className="object-contain"
+				/>
+				<p className="logo_text">Promptopia</p>
+			</Link>
+
+			{/* Desktop Navigation */}
+
+			<div className="sm:flex hidden">
+				{isUserLoggedIn ? (
+					<div className="flex gap-3 md:gap-5">
+						<Link
+							href="/create-prompt"
+							className="black_btn">
+							Create Post
+						</Link>
+					</div>
+				) : (
+					<></>
+				)}
+			</div>
+		</nav>
+	);
+};
+
+export default Nav;
