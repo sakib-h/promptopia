46174c588f4d24a95dba5da01abb9c5cda377618
Sakib Hasan <sakib100.sa@gmail.com>
2023-05-20 23:42:53 +0600

    working on create prompt page

---
+++ app/create-prompt/page.jsx
@@ -0,0 +1,10 @@
+'use client';
+import { useState } from 'react';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import Form from '@components/Form';
+const CreatePrompt = () => {
+	return <div>page</div>;
+};
+
+export default CreatePrompt;
