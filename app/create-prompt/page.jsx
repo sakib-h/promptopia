8ca6be4833e19884dde764cfd502609c76f99011
Sakib Hasan <sakib100.sa@gmail.com>
2023-05-30 20:23:16 +0600

    working on form page

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -4,7 +4,22 @@ import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 import Form from '@components/Form';
 const CreatePrompt = () => {
-	return <div>page</div>;
+	const [submitting, setSubmitting] = useState(false);
+	const [post, setPost] = useState({
+		prompt: '',
+		tag: '',
+	});
+	const createPrompt = async (e) => {};
+
+	return (
+		<Form
+			type="Create"
+			post={post}
+			setPost={setPost}
+			submitting={submitting}
+			handleSubmit={createPrompt}
+		/>
+	);
 };
 
 export default CreatePrompt;
