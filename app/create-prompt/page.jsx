266b0de2df2cd51af011f0b4737cbe84c92a4a18
Sakib Hasan <sakib100.sa@gmail.com>
2023-05-31 10:53:28 +0600

    creating Prompt model

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -9,7 +9,27 @@ const CreatePrompt = () => {
 		prompt: '',
 		tag: '',
 	});
-	const createPrompt = async (e) => {};
+	const createPrompt = async (e) => {
+		e.preventDefault();
+		setSubmitting(true);
+		try {
+			const res = await fetch('/api/prompt/new', {
+				method: 'POST',
+				body: JSON.stringify({
+					userId: session?.user.id,
+					prompt: post.prompt,
+					tag: post.tag,
+				}),
+			});
+			if (res.ok) {
+				router.push('/');
+			}
+		} catch (error) {
+			console.log(error);
+		} finally {
+			setSubmitting(false);
+		}
+	};
 
 	return (
 		<Form
